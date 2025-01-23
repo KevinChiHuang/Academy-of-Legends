56766a2ca56e2e6f840caa7a573f782a9fb87f4a
Kevin Huang <kevinchihuang@gmail.com>
2025-01-23 14:37:05 -0600

    added edit students

--- aol/myapp/urls.py
+++ aol/myapp/urls.py
@@ -5,6 +5,8 @@ from.import views
 urlpatterns = [
 
     path("", views.home, name="home"),
-    path("board/", views.board, name="board")
+    path("board/", views.board, name="board"),
+    path("shop/", views.shop, name="shop"),
+    path("editStudents/", views.editStudents, name="editStudents")
 
 ]
--- aol/myapp/views.py
+++ aol/myapp/views.py
@@ -6,3 +6,9 @@ def home(request):
 
 def board(request):
     return render(request, "board.html")
+
+def shop(request):
+    return render(request, "shop.html")
+
+def editStudents(request):
+    return render(request, "editStudents.html")
