e95a098eb888532474573cda1017d6dc6946685f
Conner Williams <98180587+Willyyy77@users.noreply.github.com>
2025-02-05 18:39:34 -0600

    fixed paths

--- aol/aol/urls.py
+++ aol/aol/urls.py
@@ -16,15 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-
-def redirect_to_login(request):
-    return redirect('login')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', redirect_to_login),
-    path('login/', include('myapp.urls')),  
+    path('', include('myapp.urls')),  
 ]
 
 
